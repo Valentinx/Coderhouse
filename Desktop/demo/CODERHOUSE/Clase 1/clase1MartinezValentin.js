b5859f3f8e3433495d730f6ce0964411d7c0f028
valentinx <valentinmartinez3009@gmail.com>
2021-09-05 20:03:05 -0300

    First Challenge

---
+++ Desktop/demo/CODERHOUSE/Clase 1/clase1MartinezValentin.js
@@ -0,0 +1,6 @@
+var numeroEntero = prompt('Ingresa un numero entero: ')
+var multiplicarEntero = prompt('Ingresa un numero entero que multiplicara al anterior: ')
+
+var multiplicacion = parseInt(numeroEntero) * parseInt(multiplicarEntero)
+
+console.log(multiplicacion)
